d06583c5089b41c2b7889a08237747094805d319
bryandms <bryandms95@gmail.com>
2020-03-23 20:12:14 -0600

    Add header & form components

---
+++ src/components/Form.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <form className="flex flex-wrap w-full">
+      <fieldset className="w-full text-center mb-8">
+        <legend>Search drinks by category or ingredient</legend>
+      </fieldset>
+
+      <div className="w-full sm:w-1/3 px-2 pb-2">
+        <input
+          className="appearance-none border-2 border-gray-300 rounded-lg w-full py-2 px-4 focus:outline-none block focus:shadow"
+          name="name"
+          type="text"
+          placeholder="Ingredient"
+        />
+      </div>
+
+      <div className="w-full sm:w-1/3 px-2 pb-2">
+        <div class="relative">
+          <select
+            className="appearance-none border-2 border-gray-300 rounded-lg w-full py-2 px-4 focus:outline-none block focus:shadow"
+            name="category"
+          >
+            <option value="">Category</option>
+          </select>
+          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg
+              class="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full sm:w-1/3 px-2 pb-2">
+        <input
+          className="appearance-none border-2 border-gray-300 rounded-lg w-full py-2 px-4 focus:outline-none block bg-red-500 text-white hover:bg-red-400 cursor-pointer"
+          type="submit"
+          value="Search"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default Form;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-red-500 py-20 px-0">
+      <h1 className="font-black text-center text-5xl text-white">
+        Search for drink recipes
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
